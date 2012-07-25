da9fbc82254da49218b4c89f2f06f09417325569
Tim Kemp <timothykemp@gmail.com>
2012-07-25 14:30:04 -0400

    Initial Commit

---
+++ Critters/TKDocument.h
@@ -0,0 +1,13 @@
+//
+//  TKDocument.h
+//  Critters
+//
+//  Created by Tim Kemp on 25/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TKDocument : NSDocument
+
+@end
---
+++ Critters/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Critters
+//
+//  Created by Tim Kemp on 25/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
