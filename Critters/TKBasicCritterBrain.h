923b74d51768bac19eb55464b07b1108be0d6fa0
Tim Kemp <timothykemp@gmail.com>
2012-08-28 16:02:13 -0400

    Using a delegate pattern for the brain to make it easier to swap out.

---
+++ Critters/TKBasicCritterBrain.h
@@ -0,0 +1,13 @@
+//
+//  TKBasicCritterBrain.h
+//  Critters
+//
+//  Created by Tim Kemp on 20/08/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TKBasicCritterBrain : NSObject
+
+@end
---
+++ Critters/TKBasicCritterBrain.m
@@ -0,0 +1,13 @@
+//
+//  TKBasicCritterBrain.m
+//  Critters
+//
+//  Created by Tim Kemp on 20/08/2012.
+//
+//
+
+#import "TKBasicCritterBrain.h"
+
+@implementation TKBasicCritterBrain
+
+@end
